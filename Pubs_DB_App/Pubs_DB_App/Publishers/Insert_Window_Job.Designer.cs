3357337c4efccd03ad39c737e40bbf90bf6ec194
Scott-Buglass <sb06260@georgiasouthern.edu>
2017-04-21 22:32:05 -0400

    Updated Insert Job Page/ Publisher WIndow
    Got insert job page up and running and got publisher window pulling
    required fields from start page

--- Pubs_DB_App/Pubs_DB_App/Publishers/Insert_Window_Job.Designer.cs
+++ Pubs_DB_App/Pubs_DB_App/Publishers/Insert_Window_Job.Designer.cs
@@ -43,13 +43,13 @@
             // 
             // tb_submit
             // 
-            this.tb_submit.Location = new System.Drawing.Point(16, 192);
-            this.tb_submit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_submit.Location = new System.Drawing.Point(12, 156);
             this.tb_submit.Name = "tb_submit";
-            this.tb_submit.Size = new System.Drawing.Size(347, 28);
+            this.tb_submit.Size = new System.Drawing.Size(260, 23);
             this.tb_submit.TabIndex = 23;
             this.tb_submit.Text = "Submit";
             this.tb_submit.UseVisualStyleBackColor = true;
+            this.tb_submit.Click += new System.EventHandler(this.tb_submit_Click);
             // 
             // groupBox1
             // 
@@ -61,95 +61,84 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.tb_description);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(16, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(347, 170);
+            this.groupBox1.Size = new System.Drawing.Size(260, 138);
             this.groupBox1.TabIndex = 21;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Job Details";
+            this.groupBox1.Text = "Job Details     * - denotes required field";
             // 
             // tb_pubID
             // 
-            this.tb_pubID.Location = new System.Drawing.Point(96, 32);
-            this.tb_pubID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_pubID.Location = new System.Drawing.Point(72, 26);
             this.tb_pubID.Name = "tb_pubID";
-            this.tb_pubID.Size = new System.Drawing.Size(231, 22);
+            this.tb_pubID.Size = new System.Drawing.Size(174, 20);
             this.tb_pubID.TabIndex = 11;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(35, 36);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(19, 29);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(50, 17);
+            this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 10;
-            this.label4.Text = "Pub ID";
+            this.label4.Text = "Pub ID *";
             // 
             // tb_maxLvl
             // 
-            this.tb_maxLvl.Location = new System.Drawing.Point(96, 128);
-            this.tb_maxLvl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_maxLvl.Location = new System.Drawing.Point(72, 104);
             this.tb_maxLvl.Name = "tb_maxLvl";
-            this.tb_maxLvl.Size = new System.Drawing.Size(231, 22);
+            this.tb_maxLvl.Size = new System.Drawing.Size(174, 20);
             this.tb_maxLvl.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 132);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(10, 107);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "Max LVL";
+            this.label2.Text = "Max LVL *";
             // 
             // tb_minLvl
             // 
-            this.tb_minLvl.Location = new System.Drawing.Point(96, 96);
-            this.tb_minLvl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_minLvl.Location = new System.Drawing.Point(72, 78);
             this.tb_minLvl.Name = "tb_minLvl";
-            this.tb_minLvl.Size = new System.Drawing.Size(231, 22);
+            this.tb_minLvl.Size = new System.Drawing.Size(174, 20);
             this.tb_minLvl.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 100);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(13, 81);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Min LVL";
+            this.label1.Text = "Min LVL *";
             // 
             // tb_description
             // 
-            this.tb_description.Location = new System.Drawing.Point(96, 64);
-            this.tb_description.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_description.Location = new System.Drawing.Point(72, 52);
             this.tb_description.Name = "tb_description";
-            this.tb_description.Size = new System.Drawing.Size(231, 22);
+            this.tb_description.Size = new System.Drawing.Size(174, 20);
             this.tb_description.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 68);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(-1, 55);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Description";
+            this.label3.Text = "Description *";
             // 
             // Insert_Window_Job
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 239);
+            this.ClientSize = new System.Drawing.Size(284, 194);
             this.Controls.Add(this.tb_submit);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Insert_Window_Job";
             this.Text = "Create New Job";
             this.Load += new System.EventHandler(this.Insert_Window_Job_Load);
