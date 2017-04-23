0adf61f0c6c514e697d0bcb0781d78a1414b2346
Russell Culpepper <russculpepper@gmail.com>
2017-04-23 14:48:21 -0400

    Add files via upload

---
+++ Pubs_DB_App/Pubs_DB_App/Stores_Sales/View_Publisher_Sales.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Pubs_DB_App.Stores_Sales
+{
+    partial class View_Publisher_Sales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_discount = new System.Windows.Forms.TextBox();
+            this.tb_quantity = new System.Windows.Forms.TextBox();
+            this.tb_orderDate = new System.Windows.Forms.TextBox();
+            this.tb_store = new System.Windows.Forms.TextBox();
+            this.tb_title = new System.Windows.Forms.TextBox();
+            this.tb_orderNum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb_discount
+            // 
+            this.tb_discount.Location = new System.Drawing.Point(100, 183);
+            this.tb_discount.Name = "tb_discount";
+            this.tb_discount.ReadOnly = true;
+            this.tb_discount.Size = new System.Drawing.Size(158, 20);
+            this.tb_discount.TabIndex = 23;
+            // 
+            // tb_quantity
+            // 
+            this.tb_quantity.Location = new System.Drawing.Point(100, 157);
+            this.tb_quantity.Name = "tb_quantity";
+            this.tb_quantity.ReadOnly = true;
+            this.tb_quantity.Size = new System.Drawing.Size(158, 20);
+            this.tb_quantity.TabIndex = 22;
+            // 
+            // tb_orderDate
+            // 
+            this.tb_orderDate.Location = new System.Drawing.Point(100, 131);
+            this.tb_orderDate.Name = "tb_orderDate";
+            this.tb_orderDate.ReadOnly = true;
+            this.tb_orderDate.Size = new System.Drawing.Size(158, 20);
+            this.tb_orderDate.TabIndex = 21;
+            // 
+            // tb_store
+            // 
+            this.tb_store.Location = new System.Drawing.Point(100, 79);
+            this.tb_store.Name = "tb_store";
+            this.tb_store.ReadOnly = true;
+            this.tb_store.Size = new System.Drawing.Size(158, 20);
+            this.tb_store.TabIndex = 20;
+            // 
+            // tb_title
+            // 
+            this.tb_title.Location = new System.Drawing.Point(100, 53);
+            this.tb_title.Name = "tb_title";
+            this.tb_title.ReadOnly = true;
+            this.tb_title.Size = new System.Drawing.Size(158, 20);
+            this.tb_title.TabIndex = 19;
+            // 
+            // tb_orderNum
+            // 
+            this.tb_orderNum.Location = new System.Drawing.Point(100, 27);
+            this.tb_orderNum.Name = "tb_orderNum";
+            this.tb_orderNum.ReadOnly = true;
+            this.tb_orderNum.Size = new System.Drawing.Size(158, 20);
+            this.tb_orderNum.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Payterms";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Quantity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Order Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Store";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Title";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Order Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 24;
+            // 
+            // View_Publisher_Sales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_discount);
+            this.Controls.Add(this.tb_quantity);
+            this.Controls.Add(this.tb_orderDate);
+            this.Controls.Add(this.tb_store);
+            this.Controls.Add(this.tb_title);
+            this.Controls.Add(this.tb_orderNum);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "View_Publisher_Sales";
+            this.Text = "View_Publisher_Sales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_discount;
+        private System.Windows.Forms.TextBox tb_quantity;
+        private System.Windows.Forms.TextBox tb_orderDate;
+        private System.Windows.Forms.TextBox tb_store;
+        private System.Windows.Forms.TextBox tb_title;
+        private System.Windows.Forms.TextBox tb_orderNum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
